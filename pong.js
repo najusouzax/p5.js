4256496704698a47c9756afd4e588d7a5b069446
najusouzax <133162354+najusouzax@users.noreply.github.com>
2023-06-14 17:17:18 -0300

    Create pong.js

---
+++ pong.js
@@ -0,0 +1,12 @@
+let xBoinha = 100; 
+let yBoliha = 200;
+let diametro = 30;
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+circle(xBoinha,yBoliha,diametro)
+} 
